023b21144e7d321502ac8071f541a04183a9be48
azaruddin92 <azaruddin92@gmail.com>
2019-03-29 17:32:39 +0530

    1 file modified

--- src/app/auth-guard.guard.ts
+++ src/app/auth-guard.guard.ts
@@ -12,22 +12,14 @@ constructor(private _authService: AuthserviceService, private _router: Router) {
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-   // return true;
-   /* if (this._authService.isAuthenticated()) {
-        return true;
-    }
-    */
-     // navigate to login page
-   // this._router.navigate(['/login']);
-    // you can save redirect url so after authing we can move them back to the page they requested
-    //return false;
+
     if(localStorage.getItem('email')!= null){
-        return true;
+          return true;
       }
       else
       {
-        this._router.navigate(['/login']);
-        return false;
+          this._router.navigate(['/login']);
+          return false;
       }
 
 
